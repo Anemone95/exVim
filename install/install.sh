fb3d5afe2075d7cb6a52916d53d0dbd3fdf95039
Anemone0 <x565178035@126.com>
2018-04-17 15:26:10 +0800

    Fix Linux install bug

--- install/install.sh
+++ install/install.sh
@@ -10,6 +10,7 @@ TOOL_DIR="$( cd "$( dirname "$0"  )" && pwd  )"
 EXVIM_DIR=$TOOL_DIR/..
 cp -r $EXVIM_DIR/vimfiles/* /usr/share/vim/vimfiles
 cp $EXVIM_DIR/.vimrc* ~/
-apt-get -y install exuberant-ctags gawk id-utils python3-jedi
+apt-get -y install exuberant-ctags gawk id-utils python3-jedi python3-pip
+pip3 install jedi
 git clone https://github.com/powerline/fonts
 source $TOOL_DIR/fonts/install.sh
