f28024ba44250d9a154fb5ba15a0dd0a561ed9d8
Anemone0 <x565178035@126.com>
2018-04-17 14:35:00 +0800

    Fix Linux install bug

--- install/install.sh
+++ install/install.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
-cp -r vimfiles/* /usr/share/vim/vimfiles
-cp .vimrc* ~/
+
+if [ "$(whoami)" != "root" ]
+then
+    echo "Please run with root!"
+    exit 1
+fi
+
+TOOL_DIR="$( cd "$( dirname "$0"  )" && pwd  )"
+EXVIM_DIR=$TOOL_DIR/..
+cp -r $EXVIM_DIR/vimfiles/* /usr/share/vim/vimfiles
+cp $EXVIM_DIR/.vimrc* ~/
 apt-get -y install exuberant-ctags gawk id-utils python3-jedi
 git clone https://github.com/powerline/fonts
-source ~/exVim/fonts/install.sh
+source $EXVIM_DIR/fonts/install.sh
