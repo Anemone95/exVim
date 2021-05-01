330a769efb8e8b7c1e052b605d27a046673adf9a
Anemone95 <x565178035@126.com>
2021-05-01 12:56:53 +0800

    21/05/01 update config, rm exvim

--- install/install.sh
+++ install/install.sh
@@ -9,11 +9,9 @@
 TOOL_DIR="$( cd "$( dirname "$0"  )" && pwd  )"
 EXVIM_DIR=$TOOL_DIR/..
 ln -s -f $EXVIM_DIR/vimfiles/ ~/.vim
-ln -f $EXVIM_DIR/.vimrc ~/.vimrc
-ln -f $EXVIM_DIR/.vimrc.local ~/.vimrc.local
-ln -f $EXVIM_DIR/.vimrc.plugins ~/.vimrc.plugins
-ln -f $EXVIM_DIR/.vimrc.plugins.local ~/.vimrc.plugins.local
-sudo apt-get -y install exuberant-ctags gawk id-utils python3-jedi python3-pip
-sudo pip3 install jedi
+ln -f $EXVIM_DIR/_vimrc ~/.vimrc
+ln -f $EXVIM_DIR/_vimrc.plugins ~/.vimrc.plugins
+sudo apt-get -y install exuberant-ctags ripgrep
+# sudo pip3 install jedi
 git clone https://github.com/powerline/fonts $TOOL_DIR/fonts
 sudo source $TOOL_DIR/fonts/install.sh
